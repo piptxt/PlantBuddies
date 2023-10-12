58b0d406c590a27d5205d3a74be73bbee8c99d6c
piptxt <andres_clemente@dlsu.edu.ph>
2023-10-13 00:44:45 +0800

    new ding dong

--- app/src/main/java/com/mobedeve/plantbuddy/MainActivity.kt
+++ app/src/main/java/com/mobedeve/plantbuddy/MainActivity.kt
@@ -7,15 +7,17 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var tvHellaur : TextView
+    private lateinit var tvDingDong : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initializeUI()
     }
-
     private fun initializeUI() {
         tvHellaur = findViewById(R.id.hellaur)
+        tvDingDong = findViewById(R.id.dingdong)
     }
+
 }
 //test
