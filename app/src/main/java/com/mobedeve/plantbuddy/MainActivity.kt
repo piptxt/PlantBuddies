b04cb2e8465202c45ab493cc7f62b1fc5e4ffa0a
piptxt <andres_clemente@dlsu.edu.ph>
2023-10-13 00:12:21 +0800

    Commit Testing!

--- app/src/main/java/com/mobedeve/plantbuddy/MainActivity.kt
+++ app/src/main/java/com/mobedeve/plantbuddy/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        class thing {
+
+        }
     }
+    // What my 9mm go bang
 }
