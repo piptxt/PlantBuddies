4c1c912fdfecf72b86d02f46589bfefd881fb354
piptxt <andres_clemente@dlsu.edu.ph>
2023-10-13 00:26:58 +0800

    Hellaur

--- app/src/main/java/com/mobedeve/plantbuddy/MainActivity.kt
+++ app/src/main/java/com/mobedeve/plantbuddy/MainActivity.kt
@@ -2,15 +2,19 @@ package com.mobedeve.plantbuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvHellaur : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        class thing {
-
-        }
+        initializeUI()
     }
-    // What my 9mm go bang
+
+    private fun initializeUI() {
+        tvHellaur = findViewById(R.id.hellaur)
+    }
 }
