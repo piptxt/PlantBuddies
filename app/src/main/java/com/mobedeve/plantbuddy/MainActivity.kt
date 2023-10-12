4fcdde1768ebcfd213374afd3216f5c6871a4729
piptxt <andres_clemente@dlsu.edu.ph>
2023-10-13 00:48:14 +0800

    something

--- app/src/main/java/com/mobedeve/plantbuddy/MainActivity.kt
+++ app/src/main/java/com/mobedeve/plantbuddy/MainActivity.kt
@@ -7,18 +7,15 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var tvHellaur : TextView
-    private lateinit var tvDingDong : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initializeUI()
     }
+
     private fun initializeUI() {
         tvHellaur = findViewById(R.id.hellaur)
-        tvDingDong = findViewById(R.id.dingdong)
     }
-
 }
 //test
-//this is the branch
